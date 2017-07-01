da82d622dd81cb17408554db208a882dc703a040
Lucas Peixoto <lucasfs@gec.inatel.br>
2017-07-01 17:12:16 -0300

    Added Create and Retrieve

--- 06-express/app/index.js
+++ 06-express/app/index.js
@@ -5,16 +5,10 @@ const bodyParser = require('body-parser')
 //setup
 const app = express()
 app.use(bodyParser.json())
-// app.use(express.static('public'))
+app.use(express.static('public'))
 
-//publish HTML5 app
-app.get('/', (request, response) => {
-    response.send('Hi')
-})
-
-app.get('/about', (request, response) => {
-    response.send('This is an express aplication')
-})
+//Subscriber API
+app.use('/api/subscribers', require('./subscriberApi'))
 
 //server startup
 app.listen(port, () => {
